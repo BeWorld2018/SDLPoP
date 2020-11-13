3e984673c5deaf87098a19354913b993cedda6f1
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-11-13 11:30:20 +0100

    Add deleted masks

--- src/config.h
+++ src/config.h
@@ -245,3 +245,15 @@ The authors of this program may be contacted at https://forum.princed.org
 #define SDL_JOYSTICK_Y_AXIS 1
 
 #endif
+
+#if defined(__amigaos4__) || defined(__MORPHOS__)
+	#define Rmsk  0x00ff0000
+	#define Gmsk  0x0000ff00
+	#define Bmsk  0x000000ff
+	#define Amsk  0xff000000
+#else
+	#define Rmsk  0x000000ff
+	#define Gmsk  0x0000ff00
+	#define Bmsk  0x00ff0000
+	#define Amsk  0xff000000
+#endif
